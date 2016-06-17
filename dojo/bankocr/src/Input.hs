d371580a2e4a96989855b334586f9897db6035aa
Felipe Lessa <felipe.lessa@gmail.com>
2016-06-17 20:49:48 -0300

    halian: inicio da funcão de 2 dígitos

--- dojo/bankocr/src/Input.hs
+++ dojo/bankocr/src/Input.hs
@@ -7,6 +7,6 @@ inputNumberOne t m b
   | t=="   " && m=="  |" && b=="  |" = 1
   | otherwise = error "inputNumberOne: cant handle this digit"
 
-inputNumberTwo = t m b
-  | t==" _ " && m=="  |" && b=="  |" = 1
-  | otherwise = error "inputNumberOne: cant handle this digit"
+inputNumberTwo t m b
+  | t==" _ " && m==" _|" && b=="|_ " = 2
+  | otherwise = error "inputNumberTwo: cant handle this digit"
