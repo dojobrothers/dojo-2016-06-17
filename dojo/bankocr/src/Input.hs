732289a8e9b83e3e9512540bfaf9437a0ab0ea3b
Felipe Lessa <felipe.lessa@gmail.com>
2016-06-17 21:05:43 -0300

    saad: terminou teste dos dois digitos

--- dojo/bankocr/src/Input.hs
+++ dojo/bankocr/src/Input.hs
@@ -11,4 +11,7 @@ inputNumberTwo t m b
   | t==" _ " && m==" _|" && b=="|_ " = 2
   | otherwise = error "inputNumberTwo: cant handle this digit"
 
-inputNumberTwelve t m b = undefined
+inputNumberTwelve t m b
+  | inputNumberOne (take 3 t) (take 3 m) (take 3 b) * 10 +
+    inputNumberTwo (drop 3 t) (drop 3 m) (drop 3 b) == 12 = 12
+  | otherwise = error "inputNumberTwelve: not twelve"
